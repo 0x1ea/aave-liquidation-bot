69d44e3f77d6953d4b9a67c5e231ff53bd186b32
Ardy <cryptoardy@gmail.com>
2022-07-22 16:23:28 -0400

    fea: format user data with their configuration

--- scripts/liquidate.js
+++ scripts/liquidate.js
@@ -1,22 +1,29 @@
 const { ethers } = require("ethers");
 const LendingPoolABI = require("../artifacts/contracts/interfaces/ILendingPool.sol/ILendingPool.json");
 const erc20ABI = require("../artifacts/contracts/interfaces/IWeth.sol/IWeth.json");
+const chainData = require("../constants/reservesList.json");
 require("dotenv").config();
 
-const chainData = require("../constants/reservesList.json");
-
+/**
+ * TO DO:
+ *  - obtener el gas antes de hacer todo
+ */
 const TOKEN_DEBT_ADDRESS = chainData.polygon[0].address;
 const TOKEN_DEBT_SYMBOL = chainData.polygon[0].symbol;
 
 const AMOUNT = ethers.utils.parseEther("0.5");
+const key = {
+  dev: "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
+  production: process.env.PRIVATE_KEY
+};
+
+const rpcUrl = {
+  dev: "http://127.0.0.1:8545/",
+  public: process.env.PUBLIC_FORK_RPC_URL,
+  production: process.env.FORK_RPC_URL
+};
 
 async function main() {
-  const privateKey = process.env.PRIVATE_KEY;
-  const FakePrivateKey =
-    "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
-
-  const RPC_PROVIDER = process.env.FORK_RPC_URL;
-  const FAKE_RPC_PROVIDER = "http://127.0.0.1:8545/";
   /**
    *
    *
@@ -26,8 +33,9 @@ async function main() {
    *
    *
    */
-  const provider = new ethers.providers.JsonRpcProvider(FAKE_RPC_PROVIDER);
-  const deployer = new ethers.Wallet(FakePrivateKey, provider);
+
+  const provider = new ethers.providers.JsonRpcProvider(rpcUrl.dev);
+  const deployer = new ethers.Wallet(key.dev, provider);
   /**
    *
    *
@@ -42,79 +50,76 @@ async function main() {
   const mainTokenAddress = wmaticAddress;
   const erc20AddressDebt = TOKEN_DEBT_ADDRESS;
 
-  console.log("My account: ", deployer.address);
-
-  await getWeth(mainTokenAddress, deployer);
-
-  const balance = await deployer.getBalance();
-  console.log("MATIC balance: ", ethers.utils.formatEther(balance));
-  await getErc20Balance(mainTokenAddress, deployer);
-  await getErc20Balance(erc20AddressDebt, deployer);
-
-  const lendingPool = await getLendingPool(deployer);
-
-  // deposit!
-  // approve
-  await approveErc20(mainTokenAddress, lendingPool.address, AMOUNT, deployer);
-  console.log("Depositing WMATIC...");
-  await lendingPool.deposit(
-    mainTokenAddress,
-    AMOUNT,
-    deployer.address,
-    0
-    /* { gasPrice: "31100000000"} */
-  );
-  console.log("Deposited!");
-
-  console.log("Pidiendo prestado el ERC20 para pagar la deuda");
-  const borrow = ethers.utils.parseEther("0.1");
-  await borrowErc20(erc20AddressDebt, lendingPool, borrow, deployer);
-
-  const midBalance = await deployer.getBalance();
-  console.log("MATIC balance: ", ethers.utils.formatEther(midBalance));
-  await getErc20Balance(mainTokenAddress, deployer);
-  const erc20Balance = await getErc20Balance(erc20AddressDebt, deployer);
-
-  console.log("Aprobando todo el usdt: ", ethers.utils.formatEther(erc20Balance));
-  await approveErc20(erc20AddressDebt, lendingPool.address, erc20Balance, deployer);
-
-  const collateralAddress = mainTokenAddress;
-  const debt = erc20AddressDebt;
-  const VICTIM_ADDRESS = "0x400F7C84ca06663b369E794f3228384E87A976d1";
-  const debtToCover = erc20Balance;
-  const receiveAToken = false;
+  console.log(`My account: ${deployer.address}\n`);
 
   console.log("Getting victim data before liquidation...");
-  await getBorrowUserData(lendingPool, VICTIM_ADDRESS);
+  const VICTIM_ADDRESS = "0x400F7C84ca06663b369E794f3228384E87A976d1";
+  const lendingPool = await getLendingPool(deployer);
+  const HF = await getBorrowUserData(lendingPool, VICTIM_ADDRESS);
 
-  await liquidateUser(
-    lendingPool,
-    collateralAddress,
-    debt,
-    VICTIM_ADDRESS,
-    debtToCover,
-    receiveAToken
-  );
+  if (HF < 1) {
+    await getWeth(mainTokenAddress, deployer);
 
-  console.log("Getting victim data after liquidation...");
-  await getBorrowUserData(lendingPool, VICTIM_ADDRESS);
+    const balance = await deployer.getBalance();
+    console.log("MATIC balance: ", ethers.utils.formatEther(balance));
+    await getErc20Balance(mainTokenAddress, deployer);
+    await getErc20Balance(erc20AddressDebt, deployer);
 
-  const newBalance = await deployer.getBalance();
+    // deposit!
+    // approve
+    await approveErc20(mainTokenAddress, lendingPool.address, AMOUNT, deployer);
+    console.log("Depositing WMATIC...");
+    await lendingPool.deposit(mainTokenAddress, AMOUNT, deployer.address, 0, {
+      gasPrice: "40100000000",
+      gasLimit: "283000"
+    });
+    console.log("Deposited!");
 
-  console.log("MATIC balance: ", ethers.utils.formatEther(newBalance));
-  await getErc20Balance(mainTokenAddress, deployer);
-  await getErc20Balance(erc20AddressDebt, deployer);
+    console.log("Pidiendo prestado el ERC20 para pagar la deuda");
+    const borrow = ethers.utils.parseEther("0.1");
+    await borrowErc20(erc20AddressDebt, lendingPool, borrow, deployer);
+
+    const midBalance = await deployer.getBalance();
+    console.log("MATIC balance: ", ethers.utils.formatEther(midBalance));
+    await getErc20Balance(mainTokenAddress, deployer);
+    const erc20Balance = await getErc20Balance(erc20AddressDebt, deployer);
+
+    console.log("Aprobando todo el usdt: ", ethers.utils.formatEther(erc20Balance));
+    await approveErc20(erc20AddressDebt, lendingPool.address, erc20Balance, deployer);
+
+    /**
+     *
+     */
+    const collateralAddress = mainTokenAddress;
+    const debt = erc20AddressDebt;
+    const debtToCover = erc20Balance;
+    const receiveAToken = true;
+
+    await liquidateUser(
+      lendingPool,
+      collateralAddress,
+      debt,
+      VICTIM_ADDRESS,
+      debtToCover,
+      receiveAToken
+    );
+
+    console.log("Getting victim data after liquidation...");
+    await getBorrowUserData(lendingPool, VICTIM_ADDRESS);
+
+    const newBalance = await deployer.getBalance();
+
+    console.log("MATIC balance: ", ethers.utils.formatEther(newBalance));
+    await getErc20Balance(mainTokenAddress, deployer);
+    await getErc20Balance(erc20AddressDebt, deployer);
+  }
 }
 
 async function borrowErc20(erc20Address, lendingPool, borrow, account) {
-  const borrowtx = await lendingPool.borrow(
-    erc20Address,
-    borrow,
-    2,
-    0,
-    account.address
-    /* { gasPrice: "31100000000" } */
-  );
+  const borrowtx = await lendingPool.borrow(erc20Address, borrow, 2, 0, account.address, {
+    gasPrice: "40100000000",
+    gasLimit: "404000"
+  });
   await borrowtx.wait(1);
   console.log(`You've borrowed!`);
 }
@@ -125,9 +130,12 @@ async function getWeth(erc20Address, account) {
   const symbol = await erc20Contract.symbol();
   const decimals = await erc20Contract.decimals();
   console.log(`Depositando ${symbol}`);
-  const tx = await erc20Contract.deposit({ value: AMOUNT });
+  const tx = await erc20Contract.deposit({
+    value: AMOUNT,
+    gasLimit: "60041",
+    gasPrice: "40100000000"
+  });
   await tx.wait(1);
-  console.log(`Obteniendo balance de ${symbol}`);
   console.log(`Got ${ethers.utils.formatUnits(balance, decimals)} ${symbol}`);
 
   return balance.toString();
@@ -169,17 +177,20 @@ async function liquidateUser(
     debt,
     VICTIM_ADDRESS,
     debtToCover,
-    receiveAToken
-    /* { gasPrice: "31100000000" } */
+    receiveAToken,
+    { gasPrice: "40100000000", gasLimit: "572000" }
   );
 
   const response = await liquidateTx.wait(1);
-  console.log(`You've Liquidated!`);
+  console.log(`You've Liquidated!\n`);
 }
 
 async function approveErc20(erc20Address, spenderAddress, amountToSpend, account) {
   const erc20Token = new ethers.Contract(erc20Address, erc20ABI.abi, account);
-  const tx = await erc20Token.approve(spenderAddress, amountToSpend);
+  const tx = await erc20Token.approve(spenderAddress, amountToSpend, {
+    gasLimit: "60000",
+    gasPrice: "40100000000"
+  });
   await tx.wait(2);
   console.log("Approved!");
 }
@@ -200,6 +211,8 @@ async function getBorrowUserData(lendingPool, account) {
   );
   console.log(`Have ${ethers.utils.formatEther(totalDebtETH)} worth of ETH borrowed.`);
   console.log(`His helthFactor is: ${formattedHF}.\n`);
+
+  return formattedHF;
 }
 
 main()
--- utils/formatUserData.js
+++ utils/formatUserData.js
@@ -1,45 +1,65 @@
 const fs = require("fs");
 const { ethers } = require("ethers");
 
+const LendingPoolABI = require("../artifacts/contracts/interfaces/ILendingPool.sol/ILendingPool.json");
+const erc20ABI = require("../artifacts/contracts/interfaces/IWeth.sol/IWeth.json");
+const chainData = require("../constants/reservesList.json");
+require("dotenv").config();
+
 // Valores que modificar antes de hacer el llamado a la funcion
 const FOLDER_NAME = "data_polygon";
 const INPUT_FILE_NAME = "users_data_polygon";
 const OUTPUT_FILE_NAME = "formatted_users_data_polygon";
 
+const key = {
+  dev: "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
+  production: process.env.PRIVATE_KEY
+};
+
+const rpcUrl = {
+  dev: "http://127.0.0.1:8545/",
+  public: process.env.PUBLIC_FORK_RPC_URL,
+  production: process.env.FORK_RPC_URL
+};
+
 async function formatUserData() {
+  const provider = new ethers.providers.JsonRpcProvider(rpcUrl.production);
+  const deployer = new ethers.Wallet(key.dev, provider);
+
+  // const wethAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+  // const wmaticAddress = "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270";
+  // const mainTokenAddress = wmaticAddress;
+  // const erc20AddressDebt = TOKEN_DEBT_ADDRESS;
+
   fs.readFile(`./${FOLDER_NAME}/${INPUT_FILE_NAME}.json`, async (err, buf) => {
     let save = buf.toString();
     const data = await JSON.parse(save);
     const newUser = [];
-    // const end = data.length;
-    // for (let i = 0; i <= end; i++) {
-    //   const info = {
-    //     user: data[i].user,
-    //     totalCollateralETH: data[i].totalCollateralETH,
-    //     formatTotalCollateralETH: ethers.utils.formatEther(data[i].totalCollateralETH),
-    //     totalDebtETH: data[i].totalDebtETH,
-    //     formatTotalDebtETH: ethers.utils.formatEther(data[i].totalDebtETH),
-    //     availableBorrowsETH: data[i].availableBorrowsETH,
-    //     healthFactor: data[i].healthFactor,
-    //     formattedHF: data[i].formattedHF
-    //   };
-    //   newUser.push(info);
-    // }
-    data.forEach(user => {
-      const info = {
-        user: user.user,
-        totalCollateralETH: user.totalCollateralETH,
-        formatTotalCollateralETH: parseFloat(
-          ethers.utils.formatEther(user.totalCollateralETH)
-        ),
-        totalDebtETH: user.totalDebtETH,
-        formatTotalDebtETH: parseFloat(ethers.utils.formatEther(user.totalDebtETH)),
-
-        healthFactor: user.healthFactor,
-        formattedHF: user.formattedHF
-      };
-      newUser.push(info);
-    });
+    const end = data.length;
+    for (let i = 0; i <= end; i++) {
+      if (data[i]?.formattedHF <= 1) {
+        const VICTIM_ADDRESS = data[i].user;
+        const lendingPool = await getLendingPool(deployer);
+        const configuration = await getUserConfiguration(lendingPool, VICTIM_ADDRESS);
+        console.log(data[i].user);
+        console.log(configuration);
+        console.log("\n");
+        const info = {
+          user: data[i].user,
+          totalCollateralETH: data[i].totalCollateralETH,
+          formatTotalCollateralETH: parseFloat(
+            ethers.utils.formatEther(data[i].totalCollateralETH)
+          ),
+          totalDebtETH: data[i].totalDebtETH,
+          formatTotalDebtETH: parseFloat(ethers.utils.formatEther(data[i].totalDebtETH)),
+          healthFactor: data[i].healthFactor,
+          formattedHF: data[i].formattedHF,
+          userConfiguration: configuration
+        };
+        newUser.push(info);
+      }
+    }
+    await data.forEach(async user => {});
 
     fs.writeFile(
       `./${FOLDER_NAME}/${OUTPUT_FILE_NAME}.json`,
@@ -51,6 +71,25 @@ async function formatUserData() {
       }
     );
   });
+}
+
+async function getUserConfiguration(lendingPool, account) {
+  const configuration = await lendingPool.getUserConfiguration(account);
+  return configuration.toString();
+}
+
+async function getLendingPool(account) {
+  /**
+   * LendingPool:
+   * Mainnet: 0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9
+   * Polygon: 0x8dFf5E27EA6b7AC08EbFdf9eB090F32ee9a30fcf
+   */
+  const lendingPool = new ethers.Contract(
+    "0x8dFf5E27EA6b7AC08EbFdf9eB090F32ee9a30fcf",
+    LendingPoolABI.abi,
+    account
+  );
+  return lendingPool;
 }
 
 formatUserData();
